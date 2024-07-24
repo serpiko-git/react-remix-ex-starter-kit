3c4ba69f44e5519aa88d682dbbf52cd1edb0e664
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2024-07-24 19:58:16 +0900

    ♻️ 불필요 파일 삭제

--- app/layout/mui/Layout.tsx
+++ app/layout/mui/Layout.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
 import { Container, Box } from '@mui/material';
+
 /**
  * import Container from '@mui/material/Container';
  * Unexpected Server Error
  * Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object.
  */
-
 import Copyright from './Copyright';
 import ProTip from './ProTip';
 export default function Layout({ children }: { children: any }) {
