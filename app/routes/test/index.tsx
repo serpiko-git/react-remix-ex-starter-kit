dd1232bd71c3eedfa85a11a2766927e4cb680dc3
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2024-08-02 11:52:40 +0900

    ✨ (app/routes/test): /test 라우터 추가, 불필요 라우터 삭제

---
+++ app/routes/test/index.tsx
@@ -0,0 +1,59 @@
+import { LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+interface Users {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+export const loader: LoaderFunction = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data: Users[] = await response.json();
+  return data;
+};
+
+export default function index() {
+  const datas: Users[] = useLoaderData();
+  return (
+    <div>
+      <h1>https://jsonplaceholder.typicode.com/users</h1>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>username</th>
+            <th>email</th>
+            <th>phone</th>
+            <th>website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.length ? (
+            datas.map((data) => (
+              <>
+                <tr>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.username}</td>
+                  <td>{data.email}</td>
+                  <td>{data.phone}</td>
+                  <td>{data.website}</td>
+                </tr>
+              </>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={6}>No Datas.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
