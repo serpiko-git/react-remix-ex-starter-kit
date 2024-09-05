2995bd8f6321a5fd88a1042271061a86ac14d02e
Heeyoung Ji <heeyoung.ji@probit.com>
2024-09-05 12:05:06 +0900

    🐛 meta tag 수정

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,8 +8,8 @@ import { SignInSide } from '~/features/sign-in-side';
  * @see {@link https://remix.run/docs/en/main/route/meta} - 라우트에 필요한 HTML 메타 태그 관리
  */
 export const meta: MetaFunction = () => [
-  { title: 'React Remix express Starter-Kit' },
-  { name: 'description', content: 'Welcome to remix!' },
+  { title: 'Julybit' },
+  { name: 'description', content: 'Julybit Back office' },
 ];
 
 // https://remix.run/docs/en/main/file-conventions/routes#basic-routes
