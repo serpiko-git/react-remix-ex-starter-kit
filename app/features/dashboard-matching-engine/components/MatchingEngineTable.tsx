05d3a54733947f22a30d919310992bfc388b7d5f
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2024-11-06 14:31:18 +0900

    ✨ (matching-engine): 매칭엔진 API 시퀀스 10개로 늘림

--- app/features/dashboard-matching-engine/components/MatchingEngineTable.tsx
+++ app/features/dashboard-matching-engine/components/MatchingEngineTable.tsx
@@ -60,7 +60,19 @@ import TableRows from './TableRows';
 //   apiProxy_v1,
 // });
 
-const _sequenceOptions = ['none', '1', '2', '3', '4'];
+const _sequenceOptions = [
+  'none',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+];
 
 const _traceFunctions: TraceFunction[] = [
   {
